b85a88a34e2e91ffa2f67bfb432cac57da66b8ea
gautham-here <gr0gautham@gmail.com>
2025-06-18 16:53:10 +0530

    Finished 1D Pipeline in Python

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 VLSI Implementation of CNN, done for CNVD, VIT Chennai under the guidance of Dr P Augusta Sophy.
 
 The document uses three images: Baboon, Cameraman and Matlab logo as well, to test the working of the different codes.
+
+## Grayscale Matrix Generation
+
+The Python code [grayscale-mat-gen.py](grayscale-mat-gen.py) converts any given image into a standard size of 25x25 pixels, of grayscale type. The two files are generated, one a row vector of size 625 as input to the 1D pipeline, and two a 25x25 matrix as input to the 2D pipeline.
+
+## 1D CNN Pipeline:
+
+Input → Convolution → ReLU → Max Pooling → Output
+
+Input: From the grayscale matrix generated
+Convolution: Kernels are imported from [kernels.py](kernels.py) and used in the pipeline for convolution.
+ReLU: Rectified linear unit, introduces non-linearity and thus allowing the network to learn complex patterns in data.
+Max Pooling: Custom Max Pooling function is created to generate the output.
+
+Output is saved at each step to be compared with Verilog.
--- kernels.py
+++ kernels.py
@@ -6,7 +6,7 @@ def identity_1d():
     return np.array([0, 1, 0], dtype=np.int8)
 
 def box_blur_1d():
-    return np.array([1, 1, 1], dtype=np.int8) 
+    return np.array([1, 1, 1], dtype=np.int8)
 
 def gaussian_blur_1d():
     return np.array([1, 2, 1], dtype=np.int8)
@@ -17,34 +17,77 @@ def sobel_1d():
 # --------- 2D KERNELS (Integer Scaled) ---------
 
 def identity_2d():
-    return np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]], dtype=np.int8)
+    return np.array([[0, 0, 0],
+                     [0, 1, 0],
+                     [0, 0, 0]], dtype=np.int8)
 
 def prewitt_h_2d():
-    return np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]], dtype=np.int8)
+    return np.array([[-1, 0, 1],
+                     [-1, 0, 1],
+                     [-1, 0, 1]], dtype=np.int8)
 
 def prewitt_v_2d():
-    return np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]], dtype=np.int8)
+    return np.array([[ 1,  1,  1],
+                     [ 0,  0,  0],
+                     [-1, -1, -1]], dtype=np.int8)
 
 def sharpening_2d():
-    return np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]], dtype=np.int8)
+    return np.array([[ 0, -1,  0],
+                     [-1,  5, -1],
+                     [ 0, -1,  0]], dtype=np.int8)
 
 def box_blur_2d():
     return np.ones((3, 3), dtype=np.int8)  # Sum = 9, normalization handled externally
 
 def gaussian_blur_2d():
-    return np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]], dtype=np.int8)  # Original /16
+    return np.array([[1, 2, 1],
+                     [2, 4, 2],
+                     [1, 2, 1]], dtype=np.int8)  # Original /16 externally
 
 def sobel_h_2d():
-    return np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=np.int8)
+    return np.array([[-1, 0, 1],
+                     [-2, 0, 2],
+                     [-1, 0, 1]], dtype=np.int8)
 
 def sobel_v_2d():
-    return np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], dtype=np.int8)
+    return np.array([[ 1,  2,  1],
+                     [ 0,  0,  0],
+                     [-1, -2, -1]], dtype=np.int8)
 
 def scharr_2d():
-    return np.array([[-3, 0, 3], [-10, 0, 10], [-3, 0, 3]], dtype=np.int8)
+    return np.array([[-3,  0,  3],
+                     [-10, 0, 10],
+                     [-3,  0,  3]], dtype=np.int8)
 
 def laplacian_2d():
-    return np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]], dtype=np.int8)
+    return np.array([[0,  1, 0],
+                     [1, -4, 1],
+                     [0,  1, 0]], dtype=np.int8)
 
 def laplacian_diag_2d():
-    return np.array([[1, 1, 1], [1, -8, 1], [1, 1, 1]], dtype=np.int8)
+    return np.array([[ 1,  1,  1],
+                     [ 1, -8,  1],
+                     [ 1,  1,  1]], dtype=np.int8)
+
+# --------- KERNEL DICTIONARIES ---------
+
+KERNELS_1D = {
+    "identity": identity_1d(),
+    "blur": box_blur_1d(),
+    "gaussian": gaussian_blur_1d(),
+    "edge": sobel_1d()
+}
+
+KERNELS_2D = {
+    "identity": identity_2d(),
+    "prewitt_h": prewitt_h_2d(),
+    "prewitt_v": prewitt_v_2d(),
+    "sharpen": sharpening_2d(),
+    "box_blur": box_blur_2d(),
+    "gaussian": gaussian_blur_2d(),
+    "sobel_h": sobel_h_2d(),
+    "sobel_v": sobel_v_2d(),
+    "scharr": scharr_2d(),
+    "laplacian": laplacian_2d(),
+    "laplacian_diag": laplacian_diag_2d()
+}
